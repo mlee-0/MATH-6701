Fix gradient calculations
- This fixes diverging loss

main.py
@@ -7,32 +7,47 @@ from data import Dataset
 
 
 def forward(x, W1, W2) -> Tuple[np.ndarray, np.ndarray]:
+    """Return the output of the model with the given inputs."""
+
+    # Layer 1.
     h1 = W1 @ np.append(x, [[[1]]], axis=1)
-    # Apply ReLU.
+    # ReLU.
     h1 *= h1 > 0
+
+    # Layer 2.
     y = W2 @ np.append(h1, [[[1]]], axis=1)
+
     return h1, y
 
-def backpropagate(loss: float, x: np.ndarray, h1: np.ndarray, W1: np.ndarray, W2: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+def backpropagate(y: np.ndarray, label: np.ndarray, x: np.ndarray, h1: np.ndarray, W1: np.ndarray, W2: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """Calculate gradients."""
+
     # Calculate gradients for the second layer.
-    G2 = 2 * loss * np.append(h1, [[[1]]], axis=1).transpose((0, 2, 1))
+    G2 = 2 * (y - label) * np.append(h1, [[[1]]], axis=1).transpose((0, 2, 1))
 
     # Calculate gradients for the first layer.
     x = np.append(x, [[[1]]], axis=1).transpose((0, 2, 1))
-    G1 = 2 * loss * (h1 > 0) * W2[:, :, :4].transpose((0, 2, 1)) * x
+    G1 = 2 * (y - label) * (h1 > 0) * W2[:, :, :4].transpose((0, 2, 1)) * x
 
     return G1, G2
 
 def mse(prediction, label):
+    """Mean squared error loss."""
+
     return np.mean((prediction - label) ** 2)
 
 def gradient_descent(W1, W2, G1, G2, learning_rate: float) -> Tuple[np.ndarray, np.ndarray]:
+    """Update and return weights."""
+
     W1 = W1 - learning_rate * G1
     W2 = W2 - learning_rate * G2
+
     return W1, W2
 
 def main(epochs: int, learning_rate: float, batch_size: int):
-    # Initialize weights.
+    """Train the model."""
+
+    # Initialize weights as small random values.
     W1 = (np.random.rand(1, 4, 3) - 0.5*0) * 1e-4
     W2 = (np.random.rand(1, 1, 5) - 0.5*0) * 1e-4
 
@@ -55,14 +70,17 @@ def main(epochs: int, learning_rate: float, batch_size: int):
 
             # Calculate loss.
             loss = mse(y, label)
+            total_loss += loss
             # Calculate gradients for the weights.
-            G1, G2 = backpropagate(loss, x, h1, W1, W2)
+            G1, G2 = backpropagate(y, label, x, h1, W1, W2)
             # Update weights using gradient descent.
             W1, W2 = gradient_descent(W1, W2, G1, G2, learning_rate)
 
-            print(f'Loss: {loss:,.2e}', end='\r')
-        print()
+            if batch % 10 == 0:
+                print(f'Training loss: {loss:,.2e}', end='\r')
+        
+        print(f'Training loss: {total_loss / dataset_size:,.2e}')
 
 
 if __name__ == '__main__':
-    main(epochs=10, learning_rate=1e-5, batch_size=1)
+    main(epochs=10, learning_rate=1e-2, batch_size=1)